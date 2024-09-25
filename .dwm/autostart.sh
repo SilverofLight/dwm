23996f46e103fae296bb245e96d8c2a9ff678367
SilverofLight <a2061686759@outlook.com>
2024-09-25 08:37:12 +0800

    fix

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -12,7 +12,6 @@ while true; do
     battery=$(acpi -b | grep -P -o '[0-9]+(?=%)')
     # 获取 WiFi 信息
     wifi=$(nmcli -t -f active,ssid dev wifi | grep '是' | cut -d':' -f2 | awk '{print $1}')
-    ip=$(nmcli device show | grep IP4 | awk 'NR==1 {print $2}'|cut -d'/' -f1)
     # 获取当前日期
     date_info=$(date "+%Y-%m-%d %H:%M:%S")
     # date_info=$(date "+%m-%d %H:%M")
@@ -21,7 +20,7 @@ while true; do
     vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d'.' -f2) 
 
     # 将信息输出到状态栏
-    xsetroot -name "/ $storage | Bat $battery% | Vol $vol% | WiFi $wifi $ip | $date_info"
+    xsetroot -name "/ $storage | Bat $battery% | Vol $vol% | WiFi $wifi | $date_info"
 
     sleep 3 
 done &
